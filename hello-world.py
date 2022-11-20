d515695587d6b58bb8d73ee781f2d06e246d2523
christianalempe <christianna.lempesi99@outlook.com>
2022-11-20 14:01:31 +0000

    Initial Commit

---
+++ hello-world.py
@@ -0,0 +1,37 @@
+'''
+
+###########################################
+
+Everything in here is a comment
+
+DATA TYPES
+
+# Strings
+# Numbers
+# Lists
+# Tuples (a list you can't change -- why? -- tuples are a bit faster?!)
+# Dictionaries
+# Bolean
+
+###########################################
+
+'''
+
+import os
+os.system('clear')
+
+firstname = 'Christiana'
+age = 22
+
+names = ["John", "Bob", "Mary"]
+names_tuple = ("John", "Bob", "Mary")
+
+fav_pizza = {
+    "John": "Pepperoni",
+    "Bob": "Mushroom",
+    "Mary": "Cheese"
+}
+
+name = True
+
+print(fav_pizza["John"])
